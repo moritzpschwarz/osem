62b2e61e47147d241509ba8960d2c3ff5ec91eb3
moritzpschwarz <moritz.schwarz@outlook.com>
2022-10-01 22:23:42 +0200

    fixed AR forecasts and expanded this to use IIS

--- R/forecast_model.R
+++ R/forecast_model.R
@@ -39,7 +39,8 @@
 forecast_model <- function(model,
                            exog_predictions = NULL,
                            n.ahead = 10,
-                           exog_fill_method = "last",
+                           exog_fill_method = "AR",
+                           ar.fill.max = 4,
                            plot.forecast = TRUE){
 
   if(class(model) != "aggmod"){stop("Forecasting only possible with an aggmod object. Execute 'run_model' to get such an object.")}
@@ -54,7 +55,7 @@ forecast_model <- function(model,
     pull(var) -> exog_vars
 
   if(is.null(exog_predictions) & exog_fill_method == "last"){
-    message("No exogenous values provided. Model will use the last available value.")
+    message("No exogenous values provided. Model will use the last available value.\nAlternative is exog_fill_method = 'AR'.")
     exog_df <- model$full_data %>%
       filter(na_item %in% exog_vars) %>%
       group_by(na_item) %>%
@@ -68,7 +69,7 @@ forecast_model <- function(model,
     exog_df %>%
       group_by(na_item) %>%
       rowwise() %>%
-      mutate(time = list(seq(time, length = n.ahead+1, by = "3 months")[1:n.ahead+1])) %>%
+      mutate(time = list(seq(time, length = n.ahead + 1, by = "3 months")[1:n.ahead + 1])) %>%
       unnest(time) %>%
       ungroup %>%
       mutate(q = lubridate::quarter(time, with_year = FALSE)) %>%
@@ -79,8 +80,8 @@ forecast_model <- function(model,
         remove_selected_columns = TRUE) -> exog_df_ready
   }
 
-  if(is.null(exog_predictions) & exog_fill_method == "AR"){
-    message("No exogenous values provided. Model will forecast the exogenous values with an AR1 process.")
+  if (is.null(exog_predictions) & exog_fill_method == "AR") {
+    message(paste0("No exogenous values provided. Model will forecast the exogenous values with an AR", ar.fill.max," process (incl. Q dummies, IIS and SIS w 't.pval = 0.001').\nAlternative is exog_fill_method = 'last'."))
 
     exog_df_intermed <- model$full_data %>%
       filter(na_item %in% exog_vars) %>%
@@ -92,22 +93,71 @@ forecast_model <- function(model,
       filter(time == max(time)) %>%
       distinct(time) %>%
       rowwise() %>%
-      mutate(time = list(seq(time, length = n.ahead+1, by = "3 months")[1:n.ahead+1])) %>%
+      mutate(time = list(seq(time, length = n.ahead + 1, by = "3 months")[1:n.ahead + 1])) %>%
       unnest(time) %>%
       ungroup
 
-    orig_mc_warning <- getOption("mc.warning")
-    options(mc.warning = FALSE)
     for(col_to_forecast in seq_along(exog_df_intermed)){
       # col_to_forecast <- 2
 
       # skip the time column
       if(col_to_forecast == 1){next}
-
       exog_df_intermed %>%
-        pull(col_to_forecast) %>%
-        arx(y = ., ar = 1, plot = TRUE) %>%
-        predict(object = ., n.ahead = n.ahead) %>%
+        mutate(q = lubridate::quarter(time, with_year = FALSE)) %>%
+        fastDummies::dummy_cols(select_columns = "q", remove_first_dummy = TRUE,remove_selected_columns = TRUE) %>%
+      arrange(time) -> to_ar_predict
+
+      to_ar_predict %>%
+        pull(col_to_forecast) -> y_ar_predict
+
+      to_ar_predict %>%
+        select(q_2, q_3, q_4) -> x_ar_predict
+
+      isat_ar_predict <- isat(y = y_ar_predict, mxreg = x_ar_predict,
+                              mc = TRUE, ar = 1:4, plot = FALSE, t.pval = 0.001,
+           print.searchinfo = FALSE, sis = TRUE, iis = TRUE)
+
+
+      # get iis dummies
+      if(!is.null(gets::isatdates(isat_ar_predict)$iis)){
+        iis_pred <- matrix(0,
+                           nrow = n.ahead,
+                           ncol = nrow(gets::isatdates(isat_ar_predict)$iis),
+                           dimnames  = list(NULL,
+                                            gets::isatdates(isat_ar_predict)$iis$breaks)) %>%
+          as_tibble()
+      }
+
+      # get sis dummies
+      if(!is.null(gets::isatdates(isat_ar_predict)$sis)){
+        sis_pred <- matrix(1,
+                           nrow = n.ahead,
+                           ncol = nrow(gets::isatdates(isat_ar_predict)$sis),
+                           dimnames  = list(NULL,
+                                            gets::isatdates(isat_ar_predict)$sis$breaks)) %>%
+          as_tibble()
+      }
+
+      model$full_data %>%
+        filter(time == max(time)) %>%
+        distinct(time) %>%
+        rowwise() %>%
+        mutate(time = list(seq(time, length = n.ahead + 1, by = "3 months")[1:n.ahead + 1])) %>%
+        unnest(time) %>%
+        ungroup %>%
+
+        mutate(q = lubridate::quarter(time, with_year = FALSE)) %>%
+        fastDummies::dummy_cols(select_columns = "q", remove_first_dummy = TRUE, remove_selected_columns = TRUE) %>%
+
+        bind_cols(.,iis_pred) %>%
+        bind_cols(.,sis_pred) %>%
+
+        select(-time) -> x_ar_predict_pred_df
+
+      rm(iis_pred)
+      rm(sis_pred)
+
+      predict(object = isat_ar_predict, n.ahead = n.ahead, newmxreg = x_ar_predict_pred_df) %>%
         as.vector -> pred_values
 
       tibble(data = pred_values) %>%
@@ -115,7 +165,6 @@ forecast_model <- function(model,
         bind_cols(exog_df_forecast,.) -> exog_df_forecast
 
     }
-    options(mc.warning = orig_mc_warning)
 
     exog_df_forecast %>%
       mutate(q = lubridate::quarter(time, with_year = FALSE)) %>%
