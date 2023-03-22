3a99c26cb2824c7da55413a0a4f162d553358294
moritzpschwarz <moritz.schwarz@outlook.com>
2023-03-22 13:45:32 +0000

    Adding a test for AR = 0

---
+++ tests/testthat/test-4-tests-without-download.R
@@ -0,0 +1,76 @@
+
+test_that("simple basic tests without download", {
+
+  spec <- dplyr::tibble(
+    type = c(
+      "d",
+      "d",
+      "n"
+    ),
+    dependent = c(
+      "StatDiscrep",
+      "TOTS",
+      "Import"
+    ),
+    independent = c(
+      "TOTS - FinConsExpHH - FinConsExpGov - GCapitalForm - Export",
+      "GValueAdd + Import",
+      "FinConsExpHH + GCapitalForm"
+    )
+  )
+
+  fa <- list(geo = "AT", s_adj = "SCA", unit = "CLV05_MEUR")
+  fb <- list(geo = "AT", s_adj = "SCA", unit = "CP_MEUR")
+  filter_list <- list("P7" = fa, "YA0" = fb, "P31_S14_S15" = fa, "P5G" = fa, "B1G" = fa, "P3_S13" = fa, "P6" = fa)
+
+  expect_silent(
+    a <- run_model(
+      specification = spec,
+      dictionary = NULL,
+      inputdata_directory = aggregate.model::sample_input,
+      filter_list = filter_list,
+      download = FALSE,
+      quiet = TRUE
+    )
+  )
+
+})
+
+test_that("simple AR tests without download", {
+
+  spec <- dplyr::tibble(
+    type = c(
+      "d",
+      "d",
+      "n"
+    ),
+    dependent = c(
+      "StatDiscrep",
+      "TOTS",
+      "Import"
+    ),
+    independent = c(
+      "TOTS - FinConsExpHH - FinConsExpGov - GCapitalForm - Export",
+      "GValueAdd + Import",
+      "FinConsExpHH + GCapitalForm"
+    )
+  )
+
+  fa <- list(geo = "AT", s_adj = "SCA", unit = "CLV05_MEUR")
+  fb <- list(geo = "AT", s_adj = "SCA", unit = "CP_MEUR")
+  filter_list <- list("P7" = fa, "YA0" = fb, "P31_S14_S15" = fa, "P5G" = fa, "B1G" = fa, "P3_S13" = fa, "P6" = fa)
+
+
+  expect_silent(
+    a <- run_model(
+      specification = spec,
+      dictionary = NULL,
+      inputdata_directory = aggregate.model::sample_input,
+      filter_list = filter_list,
+      download = FALSE,
+      quiet = TRUE,
+      AR = 0
+    )
+  )
+
+})
