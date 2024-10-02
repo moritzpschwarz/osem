1d096f82377c5d380f72c22dc1698e2583e5b578
Geoffrey Harper <geoffreyharper@Geoffreys-MacBook-Pro-2.local>
2024-10-02 00:31:03 -0700

    statcan test update

--- tests/testthat/test-5-statcan-download.R
+++ tests/testthat/test-5-statcan-download.R
@@ -1,87 +1,128 @@
-#skip_on_cran()
-skip_on_ci()
-
-#setup
-spec <- dplyr::tibble(
-  type = c(
-    "d",
-    "n"
-  ),
-  dependent = c(
-    "EmiCO2Industry",
-    "IndProd"
-  ),
-  independent = c(
-    "HICP_GAS + HICP_Energy + IndProd",
-    "GAS"
-
-  )
-)
-
-module_order <- check_config_table(spec)
-dictionary <- statcan_dict
-
-dict %>%
-  dplyr::filter(model_varname == "EmiCO2Industry") %>%
-  dplyr::mutate(geo = "CA") %>%
-  dplyr::bind_rows(dictionary,.) -> dictionary
-
 test_that('statCan_load_or_download works', {
 
-  to_obtain <- determine_variables(specification = module_order,
-                                   dictionary = dictionary)
+  skip_on_cran()
+  skip_on_ci()
+
+  spec <- dplyr::tibble(
+    type = c(
+      "d",
+      "n"
+    ),
+    dependent = c(
+      "IndProdGDP",
+      "HICP_Energy"
+    ),
+    independent = c(
+      "HICP_GAS + HICP_Energy + WORLD_OIL",
+      "IndProd"
+    )
+  )
+
+  dict_statCan <- tibble::tribble(
+    ~model_varname, ~full_name, ~database, ~variable_code, ~dataset_id, ~var_col ,~freq, ~GEO, ~geo, ~unit, ~s_adj, ~`Seasonal adjustment`, ~nace_r2, ~`North American Industry Classification System (NAICS)`, ~`North American Product Classification System (NAPCS)`,~Prices, ~`Type of fuel`, ~`Products and product groups`,~found, ~ipcc_sector, ~cpa2_1, ~siec,~ref_area,~commodity,~unit_measure,~start_period,~end_period,
+    "HICP_Energy", "Harmonised Index of Consumer Prices, Energy, index 100 = 2002", "statcan", NA,"18-10-0004-01","na_item","m","Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Energy", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "HICP_GAS", "Harmonised Index of Consumer Prices, Gas, index 100 = 2002", "statcan", NA, "18-10-0004-01", "na_item", "m", "Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Gasoline", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "GAS", "Monthly Average Retail Price for gas", "statcan", NA, "18-10-0001-01", "na_item", "m", "Canada", NA, NA, NA, NA, NA, NA, NA, NA, "Regular unleaded gasoline at self service filling stations", NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "IndProdGDP", "Industrial production [T010] in 2017 constant prices", "statcan", NA, "36-10-0434-01", "na_item" ,"m", "Canada", NA, NA, NA, "Seasonally adjusted at annual rates", NA, "Industrial production [T010]", NA, "2017 constant prices", NA, NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "IndProd", "Total, Industrial product price index (IPPI)", "statcan", NA, "18-10-0266-01", "na_item", "m", "Canada", NA, NA, NA, NA, NA, NA, "Total, Industrial product price index (IPPI)", NA, NA, NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "WORLD_OIL", "World Oil Price USD", "imf", NA, "PCPS", "na_item","M", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,"W00","POILAPSP","USD",NA,NA,
+    "FISH", "Harmonised Index of Consumer Prices, Energy, index 100 = 2002", "statcan", NA,"18-10-0004-01","na_item","m","Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Energy", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+
+  )
+  dict_statCan <- as.data.frame(dict_statCan)
+
+  dictionary <- dict_statCan #aggregate.model::statcan_dict
 
 
   actual_cols = colnames(dictionary)
-
   # basic functionality
-  data <- download_statcan(to_obtain = to_obtain,
-                           column_filters = actual_cols,
-                           quiet = FALSE)
+  module_order <- osem:::check_config_table(spec)
+  to_obtain <- osem:::determine_variables(specification = module_order,
+                                                dictionary = dictionary)
+  data <- osem:::download_statcan(to_obtain = to_obtain,
+                                             #column_filters = additional_filters,
+                                             column_filters = actual_cols,
+                                             quiet = FALSE)
 
   expect_length(data, 2)
   expect_type(data, "list")
   expect_named(data, c("df", "to_obtain"))
-  expect_true(all(data$to_obtain[which(to_obtain$database == "statcan"), "found"]))
   expect_identical(data$df %>% dplyr::filter(na_item == "HICP_GAS") %>% dplyr::pull(time) %>% lubridate::month() %>% unique() %>% sort(), c(1, 4, 7, 10))
   expect_identical(data$df %>% dplyr::filter(na_item == "HICP_Energy") %>% dplyr::pull(time) %>% lubridate::month() %>% unique() %>% sort(), c(1, 4, 7, 10))
   expect_identical(data$df %>% dplyr::filter(na_item == "IndProd") %>% dplyr::pull(time) %>% lubridate::month() %>% unique() %>% sort(), c(1, 4, 7, 10))
 
+
 })
 
 test_that('statcan_load_and_download_forecasting_functionality',{
-
-  #run the model
-  model_run <- run_model(
-    specification = spec,
-    dictionary = dictionary,
-    max.ar = 4,
-    max.dl = 4,
-    primary_source = "download",
-    quiet = TRUE
-  )
-
-  set.seed(123)
-  #forcast the model
-  expect_message(model_forecast <- forecast_model(model_run, plot = FALSE), regexp = "No exogenous values")
+  #library("tinytest")
+  #using("tinysnapshot")
+  library("readr")
+  skip_on_cran()
   skip_on_ci()
 
+  #setup
+  spec <- dplyr::tibble(
+    type = c(
+      "d",
+      "n"
+    ),
+    dependent = c(
+      "IndProdGDP",
+      "HICP_Energy"
+    ),
+    independent = c(
+      "HICP_GAS + HICP_Energy + WORLD_OIL",
+      "IndProd"
+    )
+  )
+
+  dict_statCan <- tibble::tribble(
+    ~model_varname, ~full_name, ~database, ~variable_code, ~dataset_id, ~var_col ,~freq, ~GEO, ~geo, ~unit, ~s_adj, ~`Seasonal adjustment`, ~nace_r2, ~`North American Industry Classification System (NAICS)`, ~`North American Product Classification System (NAPCS)`,~Prices, ~`Type of fuel`, ~`Products and product groups`,~found, ~ipcc_sector, ~cpa2_1, ~siec,~ref_area,~commodity,~unit_measure,~start_period,~end_period,
+    "HICP_Energy", "Harmonised Index of Consumer Prices, Energy, index 100 = 2002", "statcan", NA,"18-10-0004-01","na_item","m","Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Energy", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "HICP_GAS", "Harmonised Index of Consumer Prices, Gas, index 100 = 2002", "statcan", NA, "18-10-0004-01", "na_item", "m", "Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Gasoline", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "GAS", "Monthly Average Retail Price for gas", "statcan", NA, "18-10-0001-01", "na_item", "m", "Canada", NA, NA, NA, NA, NA, NA, NA, NA, "Regular unleaded gasoline at self service filling stations", NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "IndProdGDP", "Industrial production [T010] in 2017 constant prices", "statcan", NA, "36-10-0434-01", "na_item" ,"m", "Canada", NA, NA, NA, "Seasonally adjusted at annual rates", NA, "Industrial production [T010]", NA, "2017 constant prices", NA, NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "IndProd", "Total, Industrial product price index (IPPI)", "statcan", NA, "18-10-0266-01", "na_item", "m", "Canada", NA, NA, NA, NA, NA, NA, "Total, Industrial product price index (IPPI)", NA, NA, NA, NA, NA, NA, NA,NA,NA,NA,NA,NA,
+    "WORLD_OIL", "World Oil Price USD", "imf", NA, "PCPS", "na_item","M", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,"W00","POILAPSP","USD",NA,NA,
+    "FISH", "Harmonised Index of Consumer Prices, Energy, index 100 = 2002", "statcan", NA,"18-10-0004-01","na_item","m","Canada", NA, "units", NA, NA, NA, NA, NA, NA, NA, "Energy", NA, NA, NA, NA,NA,NA,NA,NA,NA,
+
+  )
+  #as.data.frame(dict_statCan)
+
+
+  #module_order <- aggregate.model:::check_config_table(spec)
+  dictionary <- dict_statCan #aggregate.model::statcan_dict
+
+
+  data <- "testdata/canada_imf_data/canada_imf_stable_data.csv"
+  df = read_csv(data)
+
+  #run the model
+  model_run <- run_model(specification = spec,
+                                         dictionary = dictionary,
+                                         inputdata_directory = df,
+                                         primary_source = "local")
+
+
+  #forcast the model
+  model_forecast <- forecast_model(model_run, exog_fill_method = "AR")
+
   #plot model
-  expect_s3_class(plot(model_forecast,order.as.run = TRUE),"ggplot")
+
+  expect_is(plot.osem.forecast(model_forecast,order.as.run = TRUE),class = c("gg","ggplot"))
 
   #hindcast model
   hind_cast <- forecast_insample(
     model_run,
-    sample_share = 0.9,
+    sample_share = 0.5,
     uncertainty_sample = 100,
-    exog_fill_method = "AR",
-    plot = TRUE
+    exog_fill_method = "last"
   )
-
-  expect_s3_class(hind_cast$plot,"ggplot")
+  expect_is(hind_cast$plot,class = c("gg","ggplot"))
 
 
 })
-
-##TODO
-##STRESS TESTS
+#
+# ##TODO
+# ##STRESS TESTS
