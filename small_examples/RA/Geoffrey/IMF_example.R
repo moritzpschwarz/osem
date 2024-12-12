174a872f79d4fdef1445a3d1e14155ec9ff27d93
Moritz Schwarz <moritz.schwarz@outlook.com>
2024-12-12 16:30:55 +0100

    updating test files and example

--- small_examples/RA/Geoffrey/IMF_example.R
+++ small_examples/RA/Geoffrey/IMF_example.R
@@ -1,6 +1,4 @@
-library(tidyverse)
-library(readr)
-library(magrittr)
+devtools::load_all()
 
 #toy specs for example
 spec <- dplyr::tibble(
@@ -24,36 +22,19 @@ test_imf <- tibble::tribble(
   "HICP_Energy","The TEST","imf",NA,"PCPS","na_item", NA, NA, NA, NA,"M","W00","POILAPSP","USD",NA,NA
 )
 
-bind_rows(test_imf, dict_imf) %>%
-  janitor::remove_empty("cols") %>%
-  select(-var_col) -> dict_imf_ready
-
-#dict_statCan$variable_code <- NA
-dict_imf <- as.data.frame(dict_imf)
-
-
-
-dictionary <- dict_imf #obtain dictionary
-
-
-# determine whether user has added additional filters
-default_cols <- colnames(dict_imf)
-actual_cols <- colnames(dictionary)
-additional_filters <- setdiff(actual_cols, default_cols)
-
 
 #extract variables, so that we can search for them in the statsCan database
-to_obtain <- determine_variables(specification=spec,dictionary=dictionary)
+to_obtain <- determine_variables(specification=spec,dictionary=test_imf)
 
+model_result <- run_model(specification = spec, dictionary = test_imf, primary_source = "download")
 
-model_result <- run_model(specification = spec, dictionary = dict_imf_ready, primary_source = "download")
-browser()
 model_forecast <- forecast_model(model_result, n.ahead = 10, exog_fill_method = "AR", plot = FALSE)
 
 plot <- plot(model_forecast,order.as.run = TRUE)
 
 hind_cast <- forecast_insample(
   model_result,
+  #sample_share = 0.95,
   sample_share = 0.98,
   uncertainty_sample = 100,
   exog_fill_method = "AR",
