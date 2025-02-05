d872f24fc70db1e09efe3fc7f55672c5da482ffc
Moritz Schwarz <moritz.schwarz@outlook.com>
2025-02-05 09:28:47 +0100

    modifiying the modified isat code

--- R/isat.osem.modified.R
+++ R/isat.osem.modified.R
@@ -144,8 +144,86 @@ isat.osem.modified <- function (y, mc = TRUE, ar = NULL, ewma = NULL, mxreg = NU
       archLjungB[1] <- arch.LjungB$lag
     }
   }
-  ISmatrices <- gets:::create.ISmatrices(iis = iis, sis = sis, tis = tis,
-                                         uis = uis, y.n = y.n, y.index.as.char = y.index.as.char)
+  ISmatrices <- list()
+  if (iis) {
+    mIIS <- matrix(0, y.n, y.n)
+    diag(mIIS) <- 1
+    colnames(mIIS) <- paste0("iis", y.index.as.char)
+    ISmatrices <- c(ISmatrices, list(IIS = mIIS))
+  }
+  if (sis) {
+    mSIS <- matrix(0, y.n, y.n)
+    loop.indx <- 1:y.n
+    tmp <- function(i) {
+      mSIS[i, 1:i] <<- 1
+    }
+    tmp <- sapply(loop.indx, tmp)
+    colnames(mSIS) <- paste0("sis", y.index.as.char)
+    mSIS <- mSIS[, -1]
+    ISmatrices <- c(ISmatrices, list(SIS = mSIS))
+  }
+  if (tis) {
+    mTIS <- matrix(0, y.n, y.n)
+    v1n <- seq(1, y.n)
+    loop.indx <- 1:y.n
+    tmp <- function(i) {
+      mTIS[c(i:y.n), i] <<- v1n[1:c(y.n - i + 1)]
+    }
+    tmp <- sapply(loop.indx, tmp)
+    colnames(mTIS) <- paste0("tis", y.index.as.char)
+    mTIS <- mTIS[, -1]
+    ISmatrices <- c(ISmatrices, list(TIS = mTIS))
+  }
+  if (!is.list(uis) && !identical(as.numeric(uis), 0) || is.data.frame(uis)) {
+    uis <- as.zoo(cbind(uis))
+    uis.names <- colnames(uis)
+    if (is.null(uis.names)) {
+      uis.names <- paste0("uisxreg", 1:NCOL(uis))
+    }
+    if (any(uis.names == "")) {
+      missing.colnames <- which(uis.names == "")
+      for (i in 1:length(missing.colnames)) {
+        uis.names[missing.colnames[i]] <- paste0("uisxreg",
+                                                 missing.colnames[i])
+      }
+    }
+    uis <- na.trim(uis, sides = "both", is.na = "any")
+    uis.index.as.char <- as.character(index(uis))
+    t1 <- which(uis.index.as.char == y.index.as.char[1])
+    t2 <- which(uis.index.as.char == y.index.as.char[length(y.index.as.char)])
+    uis <- coredata(uis)
+    uis <- window(uis, start = t1, end = t2)
+    uis <- cbind(coredata(as.zoo(uis)))
+    colnames(uis) <- uis.names
+    if (nrow(uis) != y.n)
+      stop("nrow(uis) is unequal to no. of observations")
+    ISmatrices <- c(ISmatrices, list(UIS = uis))
+  }
+  if (is.list(uis)) {
+    for (i in 1:length(uis)) {
+      uis[[i]] <- as.matrix(coredata(as.zoo(uis[[i]])))
+      if (nrow(uis[[i]]) != y.n) {
+        stop(paste("nrow(uis[[", i, "]]) is unequal to no. of observations",
+                   sep = ""))
+      }
+    }
+    uis.names <- paste0("UIS", 1:length(uis))
+    if (is.null(names(uis))) {
+      names(uis) <- uis.names
+    }
+    else {
+      for (i in 1:length(uis)) {
+        if (names(uis)[i] == "") {
+          names(uis)[i] <- uis.names[i]
+        }
+        else {
+          names(uis)[i] <- paste0(uis.names[i], ".",
+                                  names(uis)[i])
+        }
+      }
+    }
+    ISmatrices <- c(ISmatrices, uis)
+  }
   if (is.list(blocks)) {
     if (length(ISmatrices) != length(blocks)) {
       stop("No. of IS matrices is unequal to length(blocks)")
@@ -161,24 +239,169 @@ isat.osem.modified <- function (y, mc = TRUE, ar = NULL, ewma = NULL, mxreg = NU
   getsFun.total <- 0
   ISfinalmodels <- list()
   for (i in 1:length(ISmatrices)) {
-    result_ISMatricesLoop <- gets:::ISMatricesLoop(blocks.is.list = blocks.is.list,
-                                                   ISmatrices = ISmatrices, ratio.threshold = ratio.threshold,
-                                                   mXncol = mXncol, parallel.options = parallel.options,
-                                                   ISblocks = ISblocks, i = i, y = y, y.n = y.n, mX = mX,
-                                                   userEstArg = userEstArg, t.pval = t.pval, wald.pval = wald.pval,
-                                                   do.pet = do.pet, arLjungB = arLjungB, archLjungB = archLjungB,
-                                                   normality.JarqueB = normality.JarqueB, user.diagnostics = user.diagnostics,
-                                                   gofFunArg = gofFunArg, gof.method = gof.method,
-                                                   mxkeep = mxkeep, include.gum = include.gum, include.1cut = include.1cut,
-                                                   include.empty = include.empty, max.paths = max.paths,
-                                                   turbo = turbo, tol = tol, LAPACK = LAPACK, max.regs = max.regs,
-                                                   print.searchinfo = print.searchinfo, clusterSpec = clusterSpec,
-                                                   clusterVarlist = clusterVarlist, blocks = blocks,
-                                                   max.block.size = max.block.size, mXnames = mXnames)
-    ISblocks <- result_ISMatricesLoop$ISblocks
-    ISfinalmodels[[i]] <- result_ISMatricesLoop$ISfinalmodel
-    estimations.total <- estimations.total + result_ISMatricesLoop$estimations.counter
-    getsFun.total <- getsFun.total + result_ISMatricesLoop$getsFun.counter
+    if (!blocks.is.list) {
+      ncol.adj <- NCOL(ISmatrices[[i]])
+      if (is.null(blocks)) {
+        blockratio.value <- ncol.adj/(ratio.threshold *
+                                        ncol.adj - mXncol)
+        blocksize.value <- ncol.adj/min(y.n * ratio.threshold,
+                                        max.block.size)
+        no.of.blocks <- max(2, blockratio.value, blocksize.value)
+        no.of.blocks <- ceiling(no.of.blocks)
+        no.of.blocks <- min(ncol.adj, no.of.blocks)
+      }
+      else {
+        no.of.blocks <- blocks
+      }
+      blocksize <- ceiling(ncol.adj/no.of.blocks)
+      partitions.t2 <- blocksize
+      for (j in 1:no.of.blocks) {
+        if (blocksize * j <= ncol.adj) {
+          partitions.t2[j] <- blocksize * j
+        }
+      }
+      if (partitions.t2[length(partitions.t2)] < ncol.adj) {
+        partitions.t2 <- c(partitions.t2, ncol.adj)
+      }
+      blocksadj <- length(partitions.t2)
+      partitions.t1 <- partitions.t2 + 1
+      partitions.t1 <- c(1, partitions.t1[-blocksadj])
+      tmp <- list()
+      for (j in 1:blocksadj) {
+        tmp[[j]] <- partitions.t1[j]:partitions.t2[j]
+      }
+      ISblocks[[i]] <- tmp
+    }
+    ISblocksFun <- function(j, i, ISmatrices, ISblocks,
+                            mX, parallel.options, y, userEstArg, t.pval, wald.pval,
+                            do.pet, arLjungB, archLjungB, normality.JarqueB,
+                            user.diagnostics, gofFunArg, gof.method, mxkeep,
+                            include.gum, include.1cut, include.empty, max.paths,
+                            turbo, tol, LAPACK, max.regs, print.searchinfo) {
+      if (length(ISblocks[[i]][[j]]) == 1) {
+        tmp <- colnames(ISmatrices[[i]])[ISblocks[[i]][[j]]]
+        mXis <- cbind(ISmatrices[[i]][, ISblocks[[i]][[j]]])
+        colnames(mXis) <- tmp
+        mXis <- cbind(mX, mXis)
+      }
+      else {
+        mXis <- cbind(mX, ISmatrices[[i]][, ISblocks[[i]][[j]]])
+      }
+      mXis.names <- colnames(mXis)
+      original.mxkeep.names <- mXis.names[mxkeep]
+      mXis <- gets::dropvar(mXis, tol = tol, LAPACK = LAPACK,
+                            silent = !print.searchinfo)
+      mXis.names.afterdropvar <- colnames(mXis)
+      mxkeep.afterdropvar <- which(mXis.names.afterdropvar %in%
+                                     original.mxkeep.names)
+      if (is.null(parallel.options)) {
+        if (print.searchinfo) {
+          message("\n", appendLF = FALSE)
+          message(names(ISmatrices)[i], " block ", j,
+                  " of ", length(ISblocks[[i]]), ":", appendLF = TRUE)
+        }
+      }
+      getsis <- gets::getsFun(y, mXis, untransformed.residuals = NULL,
+                              user.estimator = userEstArg, gum.result = NULL,
+                              t.pval = t.pval, wald.pval = wald.pval, do.pet = do.pet,
+                              ar.LjungB = arLjungB, arch.LjungB = archLjungB,
+                              normality.JarqueB = normality.JarqueB, user.diagnostics = user.diagnostics,
+                              gof.function = gofFunArg, gof.method = gof.method,
+                              keep = mxkeep.afterdropvar, include.gum = include.gum,
+                              include.1cut = include.1cut, include.empty = include.empty,
+                              max.paths = max.paths, turbo = turbo, tol = tol,
+                              LAPACK = LAPACK, max.regs = max.regs, print.searchinfo = print.searchinfo,
+                              alarm = FALSE)
+      estimations.counter <<- estimations.counter + getsis$no.of.estimations
+      getsFun.counter <<- getsFun.counter + 1
+      if (is.null(getsis$specific.spec)) {
+        ISspecific.models <- NULL
+      }
+      else {
+        ISspecific.models <- names(getsis$specific.spec)
+      }
+      return(ISspecific.models)
+    }
+    estimations.counter <- 0
+    getsFun.counter <- 0
+    if (is.null(parallel.options)) {
+      ISspecific.models <- lapply(1:length(ISblocks[[i]]),
+                                  ISblocksFun, i, ISmatrices, ISblocks, mX, parallel.options,
+                                  y, userEstArg, t.pval, wald.pval, do.pet, arLjungB,
+                                  archLjungB, normality.JarqueB, user.diagnostics,
+                                  gofFunArg, gof.method, mxkeep, include.gum,
+                                  include.1cut, include.empty, max.paths, turbo,
+                                  tol, LAPACK, max.regs, print.searchinfo)
+    }
+    if (!is.null(parallel.options)) {
+      if (print.searchinfo) {
+        message("\n", appendLF = FALSE)
+        message("Preparing parallel computing...", appendLF = TRUE)
+        message(names(ISmatrices)[i], " blocks to search in parallel: ",
+                length(ISblocks[[i]]), appendLF = TRUE)
+        message("Searching...", appendLF = TRUE)
+      }
+      blocksClust <- makeCluster(clusterSpec, outfile = "")
+      clusterExport(blocksClust, clusterVarlist, envir = .GlobalEnv)
+      ISspecific.models <- parLapply(blocksClust, 1:length(ISblocks[[i]]),
+                                     ISblocksFun, i, ISmatrices, ISblocks, mX, parallel.options,
+                                     y, userEstArg, t.pval, wald.pval, do.pet, arLjungB,
+                                     archLjungB, normality.JarqueB, user.diagnostics,
+                                     gofFunArg, gof.method, mxkeep, include.gum,
+                                     include.1cut, include.empty, max.paths, turbo,
+                                     tol, LAPACK, max.regs, print.searchinfo)
+      stopCluster(blocksClust)
+    }
+    if (print.searchinfo) {
+      message("\n", appendLF = FALSE)
+      message("GETS of union of retained ", names(ISmatrices)[i],
+              " variables... ", appendLF = TRUE)
+    }
+    if (length(ISspecific.models) == 0) {
+      isNames <- NULL
+      ISfinalmodels[[i]] <- NULL
+    }
+    if (length(ISspecific.models) > 0) {
+      isNames <- NULL
+      for (j in 1:length(ISspecific.models)) {
+        if (!is.null(ISspecific.models[[j]])) {
+          isNames <- union(isNames, ISspecific.models[[j]])
+        }
+      }
+      isNames <- setdiff(isNames, mXnames)
+      if (length(isNames) == 0) {
+        ISfinalmodels[[i]] <- mXnames
+      }
+      else {
+        mXisNames <- c(mXnames, isNames)
+        mXis <- cbind(mX, ISmatrices[[i]][, isNames])
+        colnames(mXis) <- mXisNames
+        mXis.names <- colnames(mXis)
+        original.mxkeep.names <- mXis.names[mxkeep]
+        mXis <- gets::dropvar(mXis, tol = tol, LAPACK = LAPACK,
+                              silent = !print.searchinfo)
+        mXis.names.afterdropvar <- colnames(mXis)
+        mxkeep.afterdropvar <- which(mXis.names.afterdropvar %in%
+                                       original.mxkeep.names)
+        getsis <- gets::getsFun(y, mXis, untransformed.residuals = NULL,
+                                user.estimator = userEstArg, gum.result = NULL,
+                                t.pval = t.pval, wald.pval = wald.pval, do.pet = do.pet,
+                                ar.LjungB = arLjungB, arch.LjungB = archLjungB,
+                                normality.JarqueB = normality.JarqueB, user.diagnostics = user.diagnostics,
+                                gof.function = gofFunArg, gof.method = gof.method,
+                                keep = mxkeep.afterdropvar, include.gum = include.gum,
+                                include.1cut = include.1cut, include.empty = include.empty,
+                                max.paths = max.paths, turbo = turbo, tol = tol,
+                                LAPACK = LAPACK, max.regs = max.regs, print.searchinfo = print.searchinfo,
+                                alarm = FALSE)
+        estimations.counter <- estimations.counter +
+          getsis$no.of.estimations
+        getsFun.counter <- getsFun.counter + 1
+        ISfinalmodels[[i]] <- names(getsis$specific.spec)
+      }
+    }
+    estimations.total <- estimations.total + estimations.counter
+    getsFun.total <- getsFun.total + getsFun.counter
   }
   names(ISblocks) <- names(ISmatrices)
   if (print.searchinfo) {
@@ -198,28 +421,6 @@ isat.osem.modified <- function (y, mc = TRUE, ar = NULL, ewma = NULL, mxreg = NU
         colnames(tmp) <- isNames
         mIS <- cbind(mIS, tmp)
       }
-    }
-    if (NCOL(cbind(mX, mIS)) >= y.n) {
-      result_additional_blocksearch <- ISadditionalblocksearch(mXis = cbind(mX,
-                                                                            mIS), isNames = colnames(mIS), y = y, y.n = y.n,
-                                                               mX = mX, mXnames = mXnames, mxkeep = mxkeep,
-                                                               ISmatrixname = "the union of indicators", indicator.set = mIS,
-                                                               estimations.counter = estimations.counter, getsFun.counter = getsFun.counter,
-                                                               print.searchinfo = print.searchinfo, tol = tol,
-                                                               LAPACK = LAPACK, userEstArg = userEstArg, t.pval = t.pval,
-                                                               gof.method = gof.method, gofFunArg = gofFunArg,
-                                                               max.regs = max.regs, max.paths = max.paths,
-                                                               arLjungB = arLjungB, archLjungB = archLjungB,
-                                                               normality.JarqueB = normality.JarqueB, user.diagnostics = user.diagnostics,
-                                                               include.gum = include.gum, include.1cut = include.1cut,
-                                                               include.empty = include.empty, turbo = turbo,
-                                                               do.pet = do.pet, ratio.threshold = ratio.threshold,
-                                                               max.block.size = max.block.size)
-      addblocksearch.names <- colnames(result_additional_blocksearch$mXis)
-      mIS <- result_additional_blocksearch$mXis[, setdiff(mXnames,
-                                                          addblocksearch.names)]
-      estimations.counter <- result_additional_blocksearch$estimations.counter
-      getsFun.counter <- result_additional_blocksearch$getsFun.counter
     }
     mXis.names <- colnames(cbind(mX, mIS))
     original.mxkeep.names <- mXis.names[mxkeep]
